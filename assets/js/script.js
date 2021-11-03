44b8645e0218d6672496b7d7650a4fd06ce0a831
mcp321 <m_cp3@yahoo.com>
2021-11-03 09:32:34 +0000

    added first java script

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let coins = document.querySelector(".coins");
+let flipBtn = document.getElementById("flip-button");
+let resetBtn = document.getElementById("resert-button");
+let heads = 0;
+let tails = 0;
+
+flipBtn.addEventListener("click", ()=>{
+    let i = Math.floor(Math.random() * 2);
+
+    if (i)
+})
