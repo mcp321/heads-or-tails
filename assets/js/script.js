71632f0ac4b5784bab0689f0189b092b6530e89d
mcp321 <m_cp3@yahoo.com>
2021-11-03 16:35:50 +0000

    so confused rightnow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,30 @@
 let coins = document.querySelector(".coins");
-let flipBtn = document.getElementById("flip-button");
+let flipBtn = document.querySelectorAll(".flip-button");
 let resetBtn = document.getElementById("resert-button");
 let heads = document.querySelector("#heads")
 let tails = document.querySelector("#tails")
 let result;
 
-flipBtn.addEventListener("click", ()=>{
+flipBtn.forEach(button => button.addEventListener("click", (event) => { 
     let i = Math.floor(Math.random() * 2);
 
+    // get the id of the clicked button
+    const headsOrTails = event.target.dataset.coin;
+    console.log(headsOrTails)
+    if(headsOrTails == "heads" && i > 0 || headsOrTails == "tails" && i == 0){
 
-    if (i > 0 ) {
+    }
+    // if the id is the same as heads and i > 0 - the player chose heads and the flip was heads -> WIN
+    // if the id is the same as heads and i === 0 - the player choose heads, but the flip was tails
+
+    if (i > 0) {
         heads.style.display = "none";
         tails.style.display = "block";
     } else {
         heads.style.display = "block";
         tails.style.display = "none";
     }
-})
+}))
 
 
 // incremenet score function
