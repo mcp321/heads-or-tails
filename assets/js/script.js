f9a0e4abc2f13386abac41c67bc6ff21c0ad7936
mcp321 <m_cp3@yahoo.com>
2021-11-03 13:26:58 +0000

    added java to choose one coin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 let coins = document.querySelector(".coins");
 let flipBtn = document.getElementById("flip-button");
 let resetBtn = document.getElementById("resert-button");
-let heads = 0;
-let tails = 0;
+let heads = document.querySelector("#heads")
+let tails = document.querySelector("#tails")
+let result;
 
 flipBtn.addEventListener("click", ()=>{
     let i = Math.floor(Math.random() * 2);
 
-    if (i)
+
+    if (i > 0 ) {
+        heads.style.display = "none";
+        tails.style.display = "block";
+    } else {
+        heads.style.display = "block";
+        tails.style.display = "none";
+    }
 })
+
+
+// incremenet score function
