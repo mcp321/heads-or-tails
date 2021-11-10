f33abe793f9434950e612bc58c1fa9e106abd223
mcp321 <m_cp3@yahoo.com>
2021-11-10 11:32:21 +0000

    FIXED JAVA CODE MADE IT NEAT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,81 @@
+document.getElementById("flip").disabled = true;
 
 
-let buttons = document.getElementsByTagName("button")
-let flipBtn = document.getElementById("flip-button")
+function EnableFlip(shouldEnable) {
+    if (shouldEnable === 'true') {
+        document.getElementById("flip").disabled = true;
+    } else {
+        document.getElementById("flip").disabled = false;
+    }
+}
+
+function FlipButton() {
+    // Disable button and Radio buttons till we are done flipping
+    document.getElementById("flip").disabled = true;
+    document.getElementById("head").disabled = true;
+    document.getElementById("tail").disabled = true;
+
+    // Run the code to flip the coin
+    let i = Math.floor(Math.random() * 2),
+        heads = document.getElementById("heads")
+        tails = document.getElementById("tails");
+
+        if (i > 0) {
+            heads.style.display = "none";
+            tails.style.display = "block";
+        } else {
+            heads.style.display = "block";
+            tails.style.display = "none";
+        }
+
+}
 
 
 
-for (let button of buttons)
-    button.addEventListener("click", function(){ 
-        if(this.getAttribute("data-type") === "flip"){
 
-    let i = Math.floor(Math.random() * 2);
-    let heads = document.getElementById("heads")
-    let tails = document.getElementById("tails")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let buttons = document.getElementsByTagName("button")
+// let flipBtn = document.getElementById("flip-button")
+
+
+
+// for (let button of buttons)
+//     button.addEventListener("click", function(){ 
+//         if(this.getAttribute("data-type") === "flip"){
+
+//     let i = Math.floor(Math.random() * 2);
+//     let heads = document.getElementById("heads")
+//     let tails = document.getElementById("tails")
 
     
-                if (i > 0) {
-                    heads.style.display = "none";
-                    tails.style.display = "block";
-                } else {
-                    heads.style.display = "block";
-                    tails.style.display = "none";
-                }
-            
-    } else if(this.getAttribute("data-type")=== "heads"){
-        this.style.backgroundColor= "rgb(4, 119, 128)"
-
-    } else if(this.getAttribute("data-type")=== "tails"){
-        this.style.backgroundColor= "rgb(4, 119, 128)"
-
-    }else if(this.getAttribute("data-type")=== "reset")
-        this.style.backgroundColor = "rgb(4, 119, 128)"
-    }
-)
+//                 if (i > 0) {
+//                     heads.style.display = "none";
+//                     tails.style.display = "block";
+//                 } else {
+//                     heads.style.display = "block";
+//                     tails.style.display = "none";
+//                 }
         
-
+//     addScore = document.getElementById("heads-score")
+//     if (i===0 && this.getAttribute("data-type") === "heads"){
+         
         
-
-
-function tails(){
-
-}
-
-function reset(){
-
-}
+//     }else if (i > 0 && this.getAttribute("data-type") === "tails"){
+    
+                
+//     }}})
+        
